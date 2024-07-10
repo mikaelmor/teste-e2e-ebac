7ba0aad3e55562c34052bf5093cb2e8f76643b6a
Mikael Morais <mikaelmoraismacedo@gmail.com>
2024-07-10 09:49:31 -0300

    Exercicio e2e

--- exercicio-e2e.cy.js
+++ exercicio-e2e.cy.js
@@ -1,33 +1,45 @@
 /// <reference types ="cypress" />
 
-context('Exercício Testes End-to-end - Fluxo de pedido', () => {
+context('Exercicio - Testes End-to-end - Fluxos de pedido', () => {
     
-    visitarUrl() {
-        cy.visit('produtos')
-    }
+    Cypress.Commands.add('login', (usuario, senha) => 
+        cy.get('#username').type(usuario)
+        cy.get('#password').type(senha, {log: false})
+        cy.get('#password').type(senha, { log: false })
+        cy.get('.woocommerce-form > .button').click()
+    )
 
-    buscarProduto(nomeProduto) {
-        cy.get('[name="s"]').eq(1).type(nomeProduto)
-        cy.get('.button-search').eq(1).click()
-    }
+    Cypress.Commands.add('selecProduto', (tamanho, cor, quantidade) => {
+            cy.get('.post-3111 > .product-block')
+            .contains('Aero Daily Fitness Tee')
+            .click()
+            cy.get('.button-variable-item-' + tamanho).click()
+            cy.get('.button-variable-item-' + cor).click()
+            cy.get('.plus').click(quantidade)
+            cy.get('.single_add_to_cart_button').click()       
+            cy.get('.woocommerce-message > .button').click()
+            cy.get('.checkout-button').click()
 
-    buscarProdutoLista(nomeProduto) {
-        cy.get('products> .row')
-        .contains(nomeProduto)
-        .click()
-    }
+    })
+});
 
-    visitarProduto(nomeProduto) {
-        cy.visit(`produtos/${nomeProduto}`) 
-        const urlFormatada = nomeProduto.replace (/ /g, '-')
-        cy.visit(`produtos/${urlFormatada}`)
-    }
+beforeEach(() => {
+    cy.visit('/')
+});
 
-    addProdutoCarrinho(tamanho, cor,quantidade) {
-        cy.get('.button-variable-item-' + tamanho).click()
-        cy.get('.button-variable-item-' + cor).click()
-        cy.get('.plus').click(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-    } 
+it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+    
+    Cypress.Commands.add('selecCadastro', (nome,sobrenome,País,Endereço,Cidade,Estado,CEP,Telefone,Email) => 
+        cy.get('#billing_first_name').type(nome)
+        cy.get('#billing_last_name').type(sobrenome)
+        cy.get('#select2-billing_country-container').should('have.text', País)
+        cy.get('#billing_address_1').clear().type(Endereço)
+        cy.get('#billing_city').clear().type(Cidade)    
+        cy.get('#select2-billing_state-container').should('have.text', Estado)    
+        cy.get('#billing_postcode').clear().type(CEP)
+        cy.get('#billing_phone').clear().type(Telefone)
+        cy.get('#billing_email').clear().type(Email)
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
 
-}
+});
